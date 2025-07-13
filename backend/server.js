cab4ec1972e13973f8a95edef54e91ff05a3cf9a
Mandar Terkhedkar <mandarterkhedkar2.0@gmail.com>
2025-07-13 11:01:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://mern-app-kohl.vercel.app'
+}));
 app.use(express.json());
 app.use('/api/items', itemRoutes);
 app.use('/api/users', userRoutes); 
