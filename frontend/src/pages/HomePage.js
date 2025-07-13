d28baabd40d6c45ba3359eb46efe6da10950ea46
Mandar Terkhedkar <mandarterkhedkar2.0@gmail.com>
2025-07-13 10:42:53 +0530

    Update HomePage.js

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = 'https://mern-backend.onrender.com/api';
 
 function Home (props) {
     const [items, setItems] = useState([]);
